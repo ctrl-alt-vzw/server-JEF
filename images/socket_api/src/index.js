27edbd34e6a4bd96ff41dfdc3929b82dadd81289
Jan Everaert <jan@crashlab.be>
2025-01-09 12:59:17 +0100

    add lora

--- images/socket_api/src/index.js
+++ images/socket_api/src/index.js
@@ -75,7 +75,7 @@ function messageHandler(data) {
 }
 
 async function translate(text, lang, cb) {
-  await fetch(`http://${process.env.TRANSLATE_HOST}:11434/translate`, {
+  await fetch(`http://${process.env.TRANSLATE_HOST}:7500/translate`, {
     method: "POST",
     body: JSON.stringify({
       q: text,
