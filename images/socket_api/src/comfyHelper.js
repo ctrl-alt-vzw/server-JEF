3d1530adde8c425fa7ac9788eb8764d7b8f31d1e
Jan Everaert <jan@crashlab.be>
2025-01-09 09:15:02 +0100

    remove generated visual prompt:

--- images/socket_api/src/comfyHelper.js
+++ images/socket_api/src/comfyHelper.js
@@ -31,8 +31,8 @@ export const queue_prompt = (prompt_in, uuid, type, style_ref) => {
 
   flow["105"]["inputs"]["image"] = `${style_ref}`
   flow["104"]["inputs"]["weight"] = 0.4
-  flow["64"]["inputs"]["steps"] = 20
-  flow["64"]["inputs"]["cfg"] = 7
+  flow["64"]["inputs"]["steps"] = 32
+  flow["64"]["inputs"]["cfg"] = 8
   flow["10"]["inputs"]["positive"] = prompt_in + ", 2d, game asset, (top view:1.4)"
   flow["64"]["inputs"]["seed"] = Math.random() * 10000
   // flow["9"]["inputs"]["filename_prefix"] = uuid+"/"+type
