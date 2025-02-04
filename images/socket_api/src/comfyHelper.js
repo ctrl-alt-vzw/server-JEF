731acc6c5e333bfb5176e6bde5bf2fc3f0cc21a9
Jan Everaert <jan@crashlab.be>
2025-02-04 17:27:05 +0100

    add new flows

--- images/socket_api/src/comfyHelper.js
+++ images/socket_api/src/comfyHelper.js
@@ -34,7 +34,7 @@ export const queue_prompt = (prompt_in, uuid, type, style_ref) => {
   }
 
   flow["6"]["inputs"]["positive"] = prompt_in
-  flow["64"]["inputs"]["seed"] = Math.random() * 10000
+  flow["3"]["inputs"]["seed"] = Math.random() * 10000
   // flow["9"]["inputs"]["filename_prefix"] = uuid+"/"+type
   flow["12"]["inputs"]["path"] = "/home/jan/Documents/server-JEF/_volumes/images/" + uuid +"_"+type+".png"
 
