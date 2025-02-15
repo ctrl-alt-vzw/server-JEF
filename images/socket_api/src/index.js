fe9768c31ed72ce02ed4f658590f4b83c07a7fda
Jan Everaert <jan@crashlab.be>
2025-02-15 13:41:46 +0100

    flows update

--- images/socket_api/src/index.js
+++ images/socket_api/src/index.js
@@ -206,6 +206,7 @@ async function execute_prompt(id, step_handle, context) {
         const response = d.response;
         translate(response, conversations[id].language, (translated_response) => {
           try {
+            print(translated_response)
             const split_response = translated_response.split(/[\t|\n]+/g).filter(e=> e.length > 0).map((e) => cleanup_response(e))
             if(split_response.length > 2) {
 
